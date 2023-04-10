7b4b24e0661360bc649830be4a6832a1a98fb263
Ne0re0 <neo.leborgne@gmail.com>
2023-04-10 13:44:47 +0200

    remove pendu.java file

--- Pendu.java
+++
@@ -1,9 +0,0 @@
-public class Pendu {
-    public static void main(String[] args) {
-        System.out.println("Blabla");
-    }
-
-    private static void banniere(){
-        System.out.println("Banniere");
-    }
-}
